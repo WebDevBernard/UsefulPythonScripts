01571ae96b681a4a7d105c9d979c6e5669f6715d
Bernard Yang <ybernard@gmail.com>
2023-05-25 07:40:00 -0700

    add thirty_before_effective date and recompile exe

--- LoB_and_DN_Letter/LOB_DN_Template.py
+++ LoB_and_DN_Letter/LOB_DN_Template.py
@@ -2,17 +2,19 @@
 from pathlib import Path
 import pandas as pd  # pip install pandas openpyxl
 from docxtpl import DocxTemplate  # pip install docxtpl
-from datetime import datetime
+from datetime import datetime, timedelta
 
 base_dir = Path(__file__).parent.parent
 word_template_path = base_dir / "word.docx"  # name of word doc
 excel_path = base_dir / "excel.xlsx"  # name of excel
-output_dir = base_dir / datetime.today().strftime("%B %d, %Y %H%M")
-output_dir.mkdir(exist_ok=True)  # name of output folder
+output_dir = base_dir / datetime.today().strftime("%B %d, %Y %H%M") # name of output folder
+output_dir.mkdir(exist_ok=True)
 
 df = pd.read_excel(excel_path, sheet_name="Sheet1")
 
 df["effective_date"] = df["effective_date"].dt.strftime("%B %d, %Y")
+thirty_before_effective = pd.to_datetime(df["effective_date"], format="%B %d, %Y") - timedelta(days=30)
+df["thirty_before_effective"] = thirty_before_effective.dt.strftime("%B %d, %Y")
 df["today"] = datetime.today().strftime("%B %d, %Y")
 
 for record in df.to_dict(orient="records"):
