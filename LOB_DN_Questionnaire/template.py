824d74b56a0d7060fc21ca6f246f71de40cd04dc
Bernard Yang <ybernard@gmail.com>
2023-06-14 21:23:58 -0700

    removed variables

--- LOB_DN_Questionnaire/template.py
+++ LOB_DN_Questionnaire/template.py
@@ -1,5 +1,4 @@
 import pandas as pd  # pip install pandas openpyxl
-import re
 from pathlib import Path
 from docxtpl import DocxTemplate  # pip install docxtpl
 from datetime import datetime, timedelta
@@ -20,13 +19,9 @@ thirty_before_effective = pd.to_datetime(df["effective_date"], format="%B %d, %Y
 df["thirty_before_effective"] = thirty_before_effective.dt.strftime("%B %d, %Y")
 df["today"] = datetime.today().strftime("%B %d, %Y")
 
-# Adds a 0 to Wawanesa Policies
-if df['policy_number'].str.contains("Wawanesa").any() :
-    df["broker_code"] = "0" + df["broker_code"].astype(str).replace(r'\.0$', '', regex=True)
-
 # Loop generates LOB and DN
 for record in df.to_dict(orient="records"):
     doc = DocxTemplate(LOB_template_path)
     doc.render(record)
-    output_path = output_dir / f"{record['insured_name']} - Disclosure and LOB.docx"
+    output_path = output_dir / f"{record['insured_name']} - Disclosure and LOB {record['policy_number']}.docx"
     doc.save(output_path)
