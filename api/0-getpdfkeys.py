4b9b388616d686e2f821c0111773692ebeaebbf1
Bernard Yang <ybernard@gmail.com>
2024-01-17 21:21:13 -0800

    various changes

--- api/0-getpdfkeys.py
+++ api/0-getpdfkeys.py
@@ -2,26 +2,12 @@ import pprint
 from pathlib import Path
 from PyPDF2 import PdfReader
 
-pdf_filename = ["GORE - Rented Questionnaire.pdf",
-                "LOB - Family Blank.pdf",
-                "Optimum West Rental Q.pdf",
-                "Wawa Personal Information and Credit Consent Form 8871.pdf",
-                "WAWA Rental Condo Questionnaire.pdf",
-                "wawa rented dwelling Q.pdf",
-                "Binder Fee Invoice - Cedar.pdf",
-                "Wawa monthly payplan Authorization Form, Form 8003GIS.pdf"
-                ]
 base_dir = Path(__file__).parent.parent
+pdf_folder = base_dir / "input"
 
+for path in Path(pdf_folder).glob("*.pdf"):
+    data = PdfReader(path).getFormTextFields()
+    fields = [str(x) for x in data]
+    print(f"<======{path}======>")
+    pprint.pprint(data)
 
-def readPdf(pdf):
-    pdf_path = base_dir / "input" / pdf
-    return pdf_path
-
-
-for pdf in pdf_filename:
-    pdf_path = readPdf(pdf)
-    reader = PdfReader(pdf_path)
-    fields = [str(x) for x in reader.getFormTextFields()]
-    print("<========" + pdf + "========>")
-    pprint.pprint(fields)
