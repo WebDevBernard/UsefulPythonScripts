4185ef8da82e3bdb627668253782827b3deb7067
Bernard Yang <ybernard@gmail.com>
2024-01-19 23:38:49 -0800

    wip

--- api/file_completion_tool.py
+++ api/file_completion_tool.py
@@ -16,14 +16,11 @@ df["effective_date"] = pd.to_datetime(df["effective_date"]).dt.strftime("%B %d, 
 expiry_date = pd.to_datetime(df["effective_date"]) + pd.offsets.DateOffset(years=1)
 df["expiry_date"] = expiry_date.dt.strftime("%B %d, %Y")
 df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
-df[["type", "additional", "province", "postal_code"]] = df[["type", "additional", "province", "postal_code"]].astype(
-    str).apply(lambda col: col.str.upper())
-df[["named_insured", "additional_insured", "employee_name", "insurer", "street_address", "city"]] = df[
-    ["named_insured", "additional_insured", "employee_name", "insurer", "street_address", "city"]].astype(str).apply(
-    lambda col: col.str.title())
-# <================================= Replace NAN risk address =================================>
-df["risk_address"] = df["risk_address"].fillna(
-    df["street_address"] + ", " + df["city"] + ", " + df["province"] + " " + df["postal_code"])
+df[["type", "province", "postal_code"]] = df[["type", "province", "postal_code"]].astype(str).apply(lambda col: col.str.upper())
+df[["insurer", "street_address", "city"]] = df[["insurer", "street_address", "city"]].astype(str).apply(lambda x: x.str.title())
+df["mailing_address"] = df[["street_address", "city", "province", "postal_code"]].apply(lambda x: ', '.join(x[:-1]) + " " + x[-1:], axis=1)
+df["risk_address"] = df["risk_address"].fillna(df["mailing_address"])
+
 
 # PDF Writing Library
 def write_to_pdf(pdf, dictionary, rows):
@@ -34,7 +31,7 @@ def write_to_pdf(pdf, dictionary, rows):
         page = reader.getPage(pageNum)
         writer.add_page(page)
         writer.updatePageFormFieldValues(page, dictionary)
-    output_path = output_dir / f"{rows["named_insured"]}" / f"{rows["named_insured"]} {rows["type"].title()} .pdf"
+    output_path = output_dir / f"{rows["named_insured"]} {rows["type"].title()}.pdf"
     output_path.parent.mkdir(exist_ok=True)
     with open(output_path, "wb") as output_stream:
         writer.write(output_stream)
@@ -45,16 +42,27 @@ def write_to_docx(docx, rows):
     template_path = base_dir / "input" / docx
     doc = DocxTemplate(template_path)
     doc.render(rows)
-    output_path = output_dir / f"{rows["named_insured"]}" / f"{rows["named_insured"]} {rows["type"].title()} .docx"
+    output_path = output_dir / f"{rows["named_insured"]} {rows["type"].title()}.docx"
     output_path.parent.mkdir(exist_ok=True)
     doc.save(output_path)
 
 
 # Directory Paths for each file
-filename = {"CONDO RENEWAL LETTER": "RENEWAL_Condo_201711.docx"
-            }
+filename = {"CONDO RENEWAL": "COVER_Ltr_NEW_Condo_201711.docx",
+            "HOME RENEWAL": "COVER_Ltr_NEW_Homeowners_201711.docx",
+            "RENTED CONDO RENEWAL": "COVER_Ltr_NEW_RentedCondo_201711.docx",
+            "RENTED DWELLING RENEWAL": "COVER_Ltr_NEW_RentedDwelling_201711.docx",
+            "TENANTS RENEWAL": "COVER_Ltr_NEW_tenants_201711.docx"}
+
 
 for rows in df.to_dict(orient="records"):
-    rows["mailing_address"] = f"{rows["street_address"]}, {rows["city"]}, {rows["province"]} {rows["postal_code"]}"
-    if (rows["type"] == "CONDO RENEWAL LETTER"):
-        write_to_docx(filename["CONDO RENEWAL LETTER"], rows)
+    if (rows["type"] == "CONDO RENEWAL"):
+        write_to_docx(filename["CONDO RENEWAL"], rows)
+    elif (rows["type"] == "HOME RENEWAL"):
+        write_to_docx(filename["HOME RENEWAL"], rows)
+    elif (rows["type"] == "RENTED CONDO RENEWAL"):
+        write_to_docx(filename["RENTED CONDO RENEWAL"], rows)
+    elif (rows["type"] == "RENTED DWELLING RENEWAL"):
+        write_to_docx(filename["RENTED DWELLING RENEWAL"], rows)
+    elif (rows["type"] == "TENANTS RENEWAL"):
+        write_to_docx(filename["TENANTS RENEWAL"], rows)
