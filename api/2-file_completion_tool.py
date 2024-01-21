1ed08c8f31198a3d0808d4eb02b88ef4b036ad8e
Bernard Yang <ybernard@gmail.com>
2024-01-21 10:37:38 -0800

    wip

--- api/2-file_completion_tool.py
+++ api/2-file_completion_tool.py
@@ -2,7 +2,6 @@ import pandas as pd
 from pathlib import Path
 from docxtpl import DocxTemplate
 from datetime import datetime
-from PyPDF2 import PdfReader, PdfWriter
 
 base_dir = Path(__file__).parent.parent
 excel_path = base_dir / "input.xlsx"  # name of Excel
@@ -17,30 +16,15 @@ expiry_date = pd.to_datetime(df["effective_date"]) + pd.offsets.DateOffset(years
 df["expiry_date"] = expiry_date.dt.strftime("%B %d, %Y")
 df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
 df[["type", "province", "postal_code"]] = df[["type", "province", "postal_code"]].astype(str).apply(lambda col: col.str.upper())
-df[["insurer", "street_address", "city"]] = df[["insurer", "street_address", "city"]].astype(str).apply(lambda x: x.str.title())
+df[["named_insured", "insurer", "street_address", "city"]] = df[["named_insured", "insurer", "street_address", "city"]].astype(str).apply(lambda x: x.str.title())
 df["mailing_address"] = df[["street_address", "city", "province", "postal_code"]].apply(lambda x: ', '.join(x[:-1]) + " " + x[-1:], axis=1)
 df["risk_address"] = df["risk_address"].fillna(df["mailing_address"])
 
-
-# PDF Writing Library
-def write_to_pdf(pdf, dictionary, rows):
-    pdf_path = base_dir / "input" / pdf
-    reader = PdfReader(pdf_path)
-    writer = PdfWriter()
-    for pageNum in range(reader.numPages):
-        page = reader.getPage(pageNum)
-        writer.add_page(page)
-        writer.updatePageFormFieldValues(page, dictionary)
-    output_path = output_dir / f"{rows["named_insured"]} {rows["type"].title()}.pdf"
-    output_path.parent.mkdir(exist_ok=True)
-    with open(output_path, "wb") as output_stream:
-        writer.write(output_stream)
-
-
 # Word Writing Library
 def write_to_docx(docx, rows):
-    template_path = base_dir / "input" / docx
+    template_path = base_dir / "templates" / docx
     doc = DocxTemplate(template_path)
+
     doc.render(rows)
     output_path = output_dir / f"{rows["named_insured"]} {rows["type"].title()}.docx"
     output_path.parent.mkdir(exist_ok=True)
@@ -48,21 +32,17 @@ def write_to_docx(docx, rows):
 
 
 # Directory Paths for each file
-filename = {"CONDO RENEWAL": "COVER_Ltr_NEW_Condo_201711.docx",
-            "HOME RENEWAL": "COVER_Ltr_NEW_Homeowners_201711.docx",
-            "RENTED CONDO RENEWAL": "COVER_Ltr_NEW_RentedCondo_201711.docx",
-            "RENTED DWELLING RENEWAL": "COVER_Ltr_NEW_RentedDwelling_201711.docx",
-            "TENANTS RENEWAL": "COVER_Ltr_NEW_tenants_201711.docx"}
+filename = {"RENEWAL LETTER": "COVER_Ltr_NEW_RENEWAL_201711.docx"}
 
 
 for rows in df.to_dict(orient="records"):
     if (rows["type"] == "CONDO RENEWAL"):
-        write_to_docx(filename["CONDO RENEWAL"], rows)
+        write_to_docx(filename["RENEWAL LETTER"], rows)
     elif (rows["type"] == "HOME RENEWAL"):
-        write_to_docx(filename["HOME RENEWAL"], rows)
+        write_to_docx(filename["RENEWAL LETTER"], rows)
     elif (rows["type"] == "RENTED CONDO RENEWAL"):
-        write_to_docx(filename["RENTED CONDO RENEWAL"], rows)
+        write_to_docx(filename["RENEWAL LETTER"], rows)
     elif (rows["type"] == "RENTED DWELLING RENEWAL"):
-        write_to_docx(filename["RENTED DWELLING RENEWAL"], rows)
-    elif (rows["type"] == "TENANTS RENEWAL"):
-        write_to_docx(filename["TENANTS RENEWAL"], rows)
+        write_to_docx(filename["RENEWAL LETTER"], rows)
+    elif (rows["type"] == "TENANT RENEWAL"):
+        write_to_docx(filename["RENEWAL LETTER"], rows)
